071da8552bd0ef6fbd8e78779262e6008f6db2e1
Aakarsh N Rai <aakarshnrai@gmail.com>
2024-12-24 09:43:58 +0530

    aadded

--- leetcode/myatoi.cpp
+++ leetcode/myatoi.cpp
@@ -26,6 +26,8 @@ public:
         long long result = 0;
         while (i < s.size() && isdigit(s[i])) {
             result = result * 10 + (s[i] - '0');
+            // this is not wrong this will move the number 1 space to left 
+            // 5 -> 50 + 3 -> 530 + 5 -> 535. Remember bro 
 
             // Check for overflow/underflow
             if (result * sign > INT_MAX) {
