deb9ddbcf98a4ef79c034e706b3e9cdc1267f16a
Aakarsh N Rai <aakarshnrai@gmail.com>
2025-02-10 13:59:03 -0500

    Top K Frequent Elements changed list

--- leetcode/topkfreqelements.py
+++ leetcode/topkfreqelements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         old_k = {}
 
         for i in nums:
