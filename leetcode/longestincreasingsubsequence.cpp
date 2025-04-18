5517d8d73f794fc2efd519d85464246e05067564
Aakarsh N Rai <aakarshnrai@gmail.com>
2025-04-17 23:10:58 -0400

    Longest Increasing Subsequence

--- leetcode/longestincreasingsubsequence.cpp
+++ leetcode/longestincreasingsubsequence.cpp
@@ -1,6 +1,8 @@
+#include <vector>
+#include <algorithm>
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(std::vector<int>& nums) {
         std::vector<int> ret_vec;
         for (int& i : nums) {
             const auto ret = std::lower_bound(ret_vec.begin(), ret_vec.end(), i);
