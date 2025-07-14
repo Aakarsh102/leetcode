c43f027093c7f4e8c4c7f8e6a987e6be79ca8f49
Aakarsh N Rai <aakarshnrai@gmail.com>
2025-07-14 16:50:45 -0400

    Path Sum 2

--- leetcode/pathsum2.cpp
+++ leetcode/pathsum2.cpp
@@ -1,18 +1,19 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
  */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#include <vector>
 class Solution {
 public:
     std::vector<std::vector<int>> ret;
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+    std::vector<std::vector<int>> pathSum(TreeNode* root, int targetSum) {
         std::vector<int> store;
         recurse(root, 0, targetSum, store);
         
